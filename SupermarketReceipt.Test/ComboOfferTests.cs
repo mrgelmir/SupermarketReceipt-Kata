ef88897d11b42bb7748cf6117d9b2c5214db5b33
Sander Decleer <sander.decleer@novado.be>
2024-11-20 21:59:47 +0100

    Create Combo Offer

---
+++ SupermarketReceipt.Test/ComboOfferTests.cs
@@ -0,0 +1,95 @@
+﻿using Xunit;
+
+namespace SupermarketReceipt.Test;
+
+public class ComboOfferTests
+{
+    private const double ToothBrushPrice = .99;
+
+    private readonly Product toothbrush;
+    private readonly Product toothPaste;
+    private readonly ShoppingCart shoppingCart;
+
+    public ComboOfferTests()
+    {
+        toothbrush = new Product("toothBrush", ProductUnit.Each);
+        toothPaste = new Product("toothPaste", ProductUnit.Each);
+        shoppingCart = new ShoppingCart();
+    }
+
+    [Fact]
+    public void GetDiscount_ShouldBeNull_WhenRequiredProductIsMissing()
+    {
+        ComboOffer comboOffer = new(toothPaste, 2.0);
+
+        Discount discount = comboOffer.GetDiscount(toothbrush, ToothBrushPrice, 1.0, shoppingCart);
+
+        Assert.Null(discount);
+    }
+
+    [Fact]
+    public void GetDiscount_ShouldNOtBeNull_WhenRequiredProductIsAdded()
+    {
+        ComboOffer comboOffer = new(toothPaste, 2.0);
+        shoppingCart.AddItemQuantity(toothPaste, 2.0);
+
+        Discount discount = comboOffer.GetDiscount(toothbrush, ToothBrushPrice, 1.0, shoppingCart);
+
+        Assert.NotNull(discount);
+    }
+
+    [Fact]
+    public void GetDiscount_ShouldBeNull_WhenRequiredAmountIsNotMet()
+    {
+        ComboOffer comboOffer = new(toothPaste, 2.0);
+        shoppingCart.AddItemQuantity(toothPaste, 1.0);
+
+        Discount discount = comboOffer.GetDiscount(toothbrush, ToothBrushPrice, 1.0, shoppingCart);
+
+        Assert.Null(discount);
+    }
+
+    [Fact]
+    public void GetDiscount_ShouldBeFullPrice()
+    {
+        ComboOffer comboOffer = new(toothPaste, 2.0);
+        shoppingCart.AddItemQuantity(toothPaste, 2.0);
+
+        Discount discount = comboOffer.GetDiscount(toothbrush, ToothBrushPrice, 1.0, shoppingCart);
+
+        Assert.Equal(ToothBrushPrice, -discount.DiscountAmount);
+    }
+
+    [Fact]
+    public void GetDiscount_ShouldAllowMultiple()
+    {
+        ComboOffer comboOffer = new(toothPaste, 2.0);
+        shoppingCart.AddItemQuantity(toothPaste, 4.0);
+
+        Discount discount = comboOffer.GetDiscount(toothbrush, ToothBrushPrice, 2.0, shoppingCart);
+
+        Assert.Equal(2 * ToothBrushPrice, -discount.DiscountAmount);
+    }
+    
+    [Fact]
+    public void GetDiscount_IsCappedByRequiredQuantity()
+    {
+        ComboOffer comboOffer = new(toothPaste, 2.0);
+        shoppingCart.AddItemQuantity(toothPaste, 4.0);
+
+        Discount discount = comboOffer.GetDiscount(toothbrush, ToothBrushPrice, 2.0, shoppingCart);
+
+        Assert.Equal(2 * ToothBrushPrice, -discount.DiscountAmount);
+    }
+    
+    [Fact]
+    public void GetDiscount_ShouldNotAddFreeMoney()
+    {
+        ComboOffer comboOffer = new(toothPaste, 2.0);
+        shoppingCart.AddItemQuantity(toothPaste, 4.0);
+
+        Discount discount = comboOffer.GetDiscount(toothbrush, ToothBrushPrice, 1.0, shoppingCart);
+
+        Assert.Equal(ToothBrushPrice, -discount.DiscountAmount);
+    }
+}
---
+++ SupermarketReceipt/ComboOffer.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+namespace SupermarketReceipt;
+
+public class ComboOffer(Product requiredProduct, double requiredQuantity)
+{
+    public Discount GetDiscount(Product p, double unitPrice, double quantity, ShoppingCart shoppingCart)
+    {
+        ProductQuantity required = shoppingCart
+            .GetItems()
+            .FirstOrDefault(p2 => p2.Product.Equals(requiredProduct));
+
+        if (required == null || required.Quantity < requiredQuantity)
+        {
+            return null;
+        }
+
+        int maxFreeItems = (int)(required.Quantity / requiredQuantity);
+
+        int freeItems = int.Min((int)quantity, maxFreeItems);
+        return new Discount(p, "", -unitPrice * freeItems);
+    }
+}
