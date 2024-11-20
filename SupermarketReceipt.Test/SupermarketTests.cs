82d59d280ab965c7f0470da660ce5b436263abe5
Sander Decleer <sander.decleer@novado.be>
2024-11-20 21:26:23 +0100

    Add tests

--- SupermarketReceipt.Test/SupermarketTests.cs
+++ SupermarketReceipt.Test/SupermarketTests.cs
@@ -45,6 +45,138 @@ namespace SupermarketReceipt.Test
             Assert.Equal(2.5, receiptItem.Quantity);
         }
 
+        [Fact]
+        public void FiveForOne_Valid()
+        {
+            cart.AddItemQuantity(apples, 5);
+            teller.AddSpecialOffer(SpecialOfferType.FiveForAmount, apples, 9.0);
+
+            Receipt receipt = teller.ChecksOutArticlesFrom(cart);
+
+            Assert.Equal(9.0, receipt.GetTotalPrice());
+        }
+
+        [Fact]
+        public void FiveForOne_TooFew()
+        {
+            cart.AddItemQuantity(apples, 4);
+            teller.AddSpecialOffer(SpecialOfferType.FiveForAmount, apples, 9.0);
+
+            Receipt receipt = teller.ChecksOutArticlesFrom(cart);
+
+            Assert.Equal(7.96, receipt.GetTotalPrice());
+        }
+
+        [Fact]
+        public void FiveForOne_TooMuch()
+        {
+            cart.AddItemQuantity(apples, 6);
+            teller.AddSpecialOffer(SpecialOfferType.FiveForAmount, apples, 9.0);
+
+            Receipt receipt = teller.ChecksOutArticlesFrom(cart);
+
+            Assert.Equal(10.99, receipt.GetTotalPrice());
+        }
+
+        [Fact]
+        public void TenPercentOff()
+        {
+            cart.AddItemQuantity(toothbrush, 10.0);
+            teller.AddSpecialOffer(SpecialOfferType.TenPercentDiscount, toothbrush, 10.0);
+
+            Receipt receipt = teller.ChecksOutArticlesFrom(cart);
+
+            Assert.Equal(8.91, receipt.GetTotalPrice());
+        }
+
+        [Fact]
+        public void ThreeForTwo()
+        {
+            cart.AddItemQuantity(toothbrush, 3.0);
+            teller.AddSpecialOffer(SpecialOfferType.ThreeForTwo, toothbrush, 0.0);
+
+            Receipt receipt = teller.ChecksOutArticlesFrom(cart);
+
+            Assert.Equal(1.98, receipt.GetTotalPrice());
+        }
+
+        [Fact]
+        public void ThreeForTwo_TooFew()
+        {
+            cart.AddItemQuantity(toothbrush, 2.0);
+            teller.AddSpecialOffer(SpecialOfferType.ThreeForTwo, toothbrush, 0.0);
+
+            Receipt receipt = teller.ChecksOutArticlesFrom(cart);
+
+            Assert.Equal(1.98, receipt.GetTotalPrice());
+        }
+
+        [Fact]
+        public void ThreeForTwo_TooMuch()
+        {
+            cart.AddItemQuantity(toothbrush, 5.0);
+            teller.AddSpecialOffer(SpecialOfferType.ThreeForTwo, toothbrush, 0.0);
+
+            Receipt receipt = teller.ChecksOutArticlesFrom(cart);
+
+            Assert.Equal(3.96, receipt.GetTotalPrice());
+        }
+
+        [Fact]
+        public void ThreeForTwo_Double()
+        {
+            cart.AddItemQuantity(toothbrush, 6.0);
+            teller.AddSpecialOffer(SpecialOfferType.ThreeForTwo, toothbrush, 0.0);
+
+            Receipt receipt = teller.ChecksOutArticlesFrom(cart);
+
+            Assert.Equal(3.96, receipt.GetTotalPrice());
+        }
+
+        [Fact]
+        public void TwoForAmount()
+        {
+            cart.AddItemQuantity(toothbrush, 2.0);
+            teller.AddSpecialOffer(SpecialOfferType.TwoForAmount, toothbrush, 1.5);
+
+            Receipt receipt = teller.ChecksOutArticlesFrom(cart);
+
+            Assert.Equal(1.5, receipt.GetTotalPrice());
+        }
+
+        [Fact]
+        public void TwoForAmount_TooFew()
+        {
+            cart.AddItemQuantity(toothbrush, 1.0);
+            teller.AddSpecialOffer(SpecialOfferType.TwoForAmount, toothbrush, 1.5);
+
+            Receipt receipt = teller.ChecksOutArticlesFrom(cart);
+
+            Assert.Equal(.99, receipt.GetTotalPrice());
+        }
+
+        [Fact]
+        public void TwoForAmount_TooMuch()
+        {
+            cart.AddItemQuantity(toothbrush, 3.0);
+            teller.AddSpecialOffer(SpecialOfferType.TwoForAmount, toothbrush, 1.5);
+
+            Receipt receipt = teller.ChecksOutArticlesFrom(cart);
+
+            Assert.Equal(1.5 + .99, receipt.GetTotalPrice());
+        }
+
+        [Fact]
+        public void TwoForAmount_Double()
+        {
+            cart.AddItemQuantity(toothbrush, 4.0);
+            teller.AddSpecialOffer(SpecialOfferType.TwoForAmount, toothbrush, 1.5);
+
+            Receipt receipt = teller.ChecksOutArticlesFrom(cart);
+
+            Assert.Equal(3.0, receipt.GetTotalPrice());
+        }
+        
         private void PopulateCatalog()
         {
             catalog.AddProduct(toothbrush, 0.99);
