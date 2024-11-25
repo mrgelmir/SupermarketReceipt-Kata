b8c9203125ad693d926ae106f3cff277a14e8775
Sander Decleer <sander.decleer@novado.be>
2024-11-25 14:32:41 +0100

    Start extracting offer

--- SupermarketReceipt/Offer.cs
+++ SupermarketReceipt/Offer.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace SupermarketReceipt
 {
     public enum SpecialOfferType
@@ -12,11 +14,70 @@ namespace SupermarketReceipt
     {
         public Offer(SpecialOfferType offerType, double argument)
         {
-            OfferType = offerType;
-            Argument = argument;
+            this.offerType = offerType;
+            this.argument = argument;
         }
 
-        public SpecialOfferType OfferType { get; }
-        public double Argument { get; }
+        private readonly SpecialOfferType offerType;
+        private readonly double argument;
+
+        public Discount GetDiscount(Product p, double unitPrice, double quantity, ShoppingCart shoppingCart,
+            CultureInfo culture)
+        {
+            Discount discount = null;
+
+            if (offerType == SpecialOfferType.TwoForAmount)
+            {
+                int quantityAsInt = (int)quantity;
+
+                if (quantityAsInt >= 2)
+                {
+                    const int discountCountItems = 2;
+                    double total = argument * (quantityAsInt / discountCountItems) + quantityAsInt % 2 * unitPrice;
+                    double discountN = unitPrice * quantity - total;
+                    discount = new Discount(p, "2 for " + PrintPrice(argument, culture), -discountN);
+                }
+            }
+
+            if (offerType == SpecialOfferType.ThreeForTwo)
+            {
+                int quantityAsInt = (int)quantity;
+
+                if (quantityAsInt > 2)
+                {
+                    const int discountCountItems = 3;
+                    int numberOfXs = quantityAsInt / discountCountItems;
+                    double discountAmount = quantity * unitPrice
+                        - (numberOfXs * 2 * unitPrice + quantityAsInt % 3 * unitPrice);
+                    discount = new Discount(p, "3 for 2", -discountAmount);
+                }
+            }
+
+            if (offerType == SpecialOfferType.TenPercentDiscount)
+            {
+                discount = new Discount(p, argument + "% off", -quantity * unitPrice * argument / 100.0);
+            }
+
+            if (offerType == SpecialOfferType.FiveForAmount)
+            {
+                int quantityAsInt = (int)quantity;
+                if (quantityAsInt >= 5)
+                {
+                    const int discountCountItems = 5;
+                    int numberOfXs = quantityAsInt / discountCountItems;
+                    double discountTotal =
+                        unitPrice * quantity - (argument * numberOfXs + quantityAsInt % 5 * unitPrice);
+                    discount = new Discount(p, discountCountItems + " for " + PrintPrice(argument, culture),
+                        -discountTotal);
+                }
+            }
+
+            return discount;
+        }
+
+        private string PrintPrice(double price, CultureInfo culture)
+        {
+            return price.ToString("N2", culture);
+        }
     }
 }
--- SupermarketReceipt/ShoppingCart.cs
+++ SupermarketReceipt/ShoppingCart.cs
@@ -45,37 +45,7 @@ namespace SupermarketReceipt
                 {
                     var offer = offers[p];
                     var unitPrice = catalog.GetUnitPrice(p);
-                    Discount discount = null;
-                    var x = 1;
-                    if (offer.OfferType == SpecialOfferType.ThreeForTwo)
-                    {
-                        x = 3;
-                    }
-                    else if (offer.OfferType == SpecialOfferType.TwoForAmount)
-                    {
-                        x = 2;
-                        if (quantityAsInt >= 2)
-                        {
-                            var total = offer.Argument * (quantityAsInt / x) + quantityAsInt % 2 * unitPrice;
-                            var discountN = unitPrice * quantity - total;
-                            discount = new Discount(p, "2 for " + PrintPrice(offer.Argument), -discountN);
-                        }
-                    }
-
-                    if (offer.OfferType == SpecialOfferType.FiveForAmount) x = 5;
-                    var numberOfXs = quantityAsInt / x;
-                    if (offer.OfferType == SpecialOfferType.ThreeForTwo && quantityAsInt > 2)
-                    {
-                        var discountAmount = quantity * unitPrice - (numberOfXs * 2 * unitPrice + quantityAsInt % 3 * unitPrice);
-                        discount = new Discount(p, "3 for 2", -discountAmount);
-                    }
-
-                    if (offer.OfferType == SpecialOfferType.TenPercentDiscount) discount = new Discount(p, offer.Argument + "% off", -quantity * unitPrice * offer.Argument / 100.0);
-                    if (offer.OfferType == SpecialOfferType.FiveForAmount && quantityAsInt >= 5)
-                    {
-                        var discountTotal = unitPrice * quantity - (offer.Argument * numberOfXs + quantityAsInt % 5 * unitPrice);
-                        discount = new Discount(p, x + " for " + PrintPrice(offer.Argument), -discountTotal);
-                    }
+                    Discount discount = offer.GetDiscount(p, unitPrice, quantity, this, Culture);
 
                     if (discount != null)
                         receipt.AddDiscount(discount);
